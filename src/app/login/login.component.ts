93497719771ba9b4f5871bc59807902e9d9b6d54
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-07-09 09:22:16 -0300

    alt4

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,17 +8,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  nome = '';
-  senha = '';
+  nome: string = '';
+  senha: string = '';
   lembrar: boolean = false;
+  errorMessage: string = '';
 
   constructor(private router: Router) {}
 
   fazerLogin(): void {
+    this.errorMessage = '';
+
     if (this.nome === 'admin' && this.senha === '123456') {
+      console.log('Login bem-sucedido! Redirecionando para /home...');
+      if (this.lembrar) {
+        console.log('Opção "Logar automaticamente" selecionada.');
+      }
       this.router.navigate(['/home']);
     } else {
-      alert('Usuário ou senha inválidos');
+      console.log(
+        'Tentativa de login falhou. Usuário:',
+        this.nome,
+        'Senha:',
+        this.senha
+      );
+      this.errorMessage = 'Usuário ou senha inválidos.';
     }
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-  ],
-  exports: [LoginComponent]
+  declarations: [LoginComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [LoginComponent],
 })
-export class LoginModule { }
+export class LoginModule {}
