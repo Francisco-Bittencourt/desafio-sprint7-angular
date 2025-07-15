5c9a14433dd07e0b19cc47309525a723f69c3d89
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-07-15 11:34:00 -0300

    alt0.5

---
+++ src/app/test-drive/test-drive.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestDriveRoutingModule } from './test-drive-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TestDriveRoutingModule
+  ]
+})
+export class TestDriveModule { }
