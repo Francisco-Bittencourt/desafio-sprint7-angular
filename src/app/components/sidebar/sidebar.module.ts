0e7252df9676199674abf9ca87446774e2aa4e93
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-07-16 08:08:37 -0300

    alt1.5

--- src/app/components/sidebar/sidebar.module.ts
+++ src/app/components/sidebar/sidebar.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
-import { RouterModule } from '@angular/router'; // Importar RouterModule aqui
+import { RouterLink} from '@angular/router'; // Importar RouterModule aqui
 
 @NgModule({
   declarations: [SidebarComponent],
   imports: [
     CommonModule,
-    RouterModule // Adicionado para que a diretiva routerLink funcione
+    RouterLink  // Adicionado para que a diretiva routerLink funcione
   ],
   exports: [SidebarComponent],
 })
