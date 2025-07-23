660bb67629fb6088379701ec2bdea89f8eef41fb
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-07-23 10:37:32 -0300

    environments

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api-ford-gqoj.onrender.com',
-    debugMode: false,
-  };
+  production: true,
+  apiUrl: 'https://api-ford-gqoj.onrender.com',
+  debugMode: false,
+};
